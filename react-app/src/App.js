f47f03781a5ea667d19225530be27b79809ee6aa
Madhusuthanan-B <madhub081011@gmail.com>
2021-03-17 12:10:04 +0530

    Integrating web components with react app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
+            <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +19,33 @@ function App() {
           Learn React
         </a>
       </header>
+      <h2>Sample React app</h2>
+      <div className="demo-section">
+        <core-btn color="primary">Primary</core-btn>
+        <core-btn color="secondary">Secondary</core-btn>
+        <core-btn color="error">Error</core-btn>
+        <core-btn color="warn">Warn</core-btn>
+        <core-btn color="info">Info</core-btn>
+      </div>
+      <div className="demo-section">
+        <core-spinner></core-spinner>
+        <core-spinner type="border" color="primary"></core-spinner>
+        <core-spinner type="border" color="secondary"></core-spinner>
+        <core-spinner type="border" color="error"></core-spinner>
+        <core-spinner type="border" color="warn"></core-spinner>
+        <core-spinner type="border" color="info"></core-spinner>
+      </div>
+      <div className="demo-section">
+        <core-card header="Card header" subheader="Sub Header">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, quam enim! Vel neque esse qui beatae
+          aliquam, praesentium non nobis odit velit veniam error quo laboriosam enim harum. Laborum, error!
+          Quae debitis quasi illum, optio eius at eaque molestias quo quam fugit reiciendis a eum adipisci officiis
+          voluptate nobis atque magnam suscipit consectetur dolorem. Porro aspernatur sit nihil quasi doloremque.
+          Molestiae, itaque aspernatur. Et nam velit incidunt, debitis eos illum ipsum, enim illo eum rem asperiores.
+          Aliquid provident maiores deserunt facilis, delectus, suscipit dolore, impedit sit nemo distinctio porro
+            enim!</p>
+        </core-card>
+      </div>
     </div>
   );
 }
