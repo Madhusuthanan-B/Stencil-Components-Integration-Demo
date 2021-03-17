6574a8b0cc4c2768552f2b228cc2e7daa3e0a7b3
Madhusuthanan-B <madhub081011@gmail.com>
2021-03-17 21:55:22 +0530

    More demo pages

--- angular-app/src/app/drop-down-demo-page/drop-down-demo-page.component.ts
+++ angular-app/src/app/drop-down-demo-page/drop-down-demo-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IDropDownOption } from '@foo/core-library/dist/types/components/drop-down/drop-down.model';
 
 @Component({
   selector: 'app-drop-down-demo-page',
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drop-down-demo-page.component.scss']
 })
 export class DropDownDemoPageComponent implements OnInit {
+  dropDownOptions: IDropDownOption[] = [
+    {
+      name: 'Vessel 1',
+      value: 'moller-maersk'
+    },
+    {
+      name: 'Vessel 2',
+      value: 'mediterranean-ship-company'
+    },
+    {
+      name: 'Vessel 3',
+      value: 'cma-cgm-group'
+    },
+    {
+      name: 'Vessel 4',
+      value: 'china-ocean-shipping-group'
+    }
+  ];
 
   constructor() { }
 
--- angular-app/src/app/list-group-demo-page/list-group-demo-page.component.ts
+++ angular-app/src/app/list-group-demo-page/list-group-demo-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IListGroupOption } from '@foo/core-library/dist/types/components/list-group/list-group.model';
 
 @Component({
   selector: 'app-list-group-demo-page',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-group-demo-page.component.scss']
 })
 export class ListGroupDemoPageComponent implements OnInit {
+  listOptions: IListGroupOption[] = [
+    {
+      name: 'A.P. Moller–Maersk Group. TEU: 3,012,172',
+      value: 'moller-maersk'
+    },
+    {
+      name: 'Mediterranean Shipping Company S.A. (MSC) TEU',
+      value: 'mediterranean-ship-company'
+    },
+    {
+      name: 'CMA CGM Group',
+      value: 'cma-cgm-group'
+    },
+    {
+      name: 'China Ocean Shipping (Group) Company (COSCO)',
+      value: 'china-ocean-shipping-group'
+    },
+    {
+      name: 'Hamburg Süd Group. TEU: 646,918',
+      value: 'hamburg-sud-group'
+    },
+    {
+      name: 'Hanjin Shipping. TEU: 621,243',
+      value: 'hanjin-shipping'
+    }
+  ]
 
   constructor() { }
 
